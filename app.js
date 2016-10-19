df2b61439ced242b095937c2268b61b3a075e919
Suresh Shrestha <sureshshrestha@lftechnology.com>
2016-10-19 13:16:12 +0545

    2.4 Static web server.

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello World');
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/about', function(req, res) {
-    res.send('<h1>About Us</h1>');
+    res.sendFile(path.join(__dirname, 'about.html'));
+});
+
+app.get('/services', function(req, res) {
+    res.sendFile(path.join(__dirname, 'services.html'));
 });
 
 app.listen(3000, function(){
